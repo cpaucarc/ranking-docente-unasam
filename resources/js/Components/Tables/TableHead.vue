9c98ffffd0e84ca8ccb0296780dc86a131d2b1e0
Frank Paucar <fcpaucar@gmail.com>
2022-10-13 17:48:25 -0500

    :sparkles: agrega el componente de tabla

---
+++ resources/js/Components/Tables/TableHead.vue
@@ -0,0 +1,18 @@
+<template>
+    <th :class="`px-3 py-3 font-bold text-gray-400 tracking-wide leading-4 ${clase_hide}`">
+        <slot/>
+    </th>
+</template>
+
+<script setup>
+const props = defineProps({
+    hideOn: {type: [Boolean, String], default: false}
+})
+
+const clase_hide = props.hideOn ? {
+    'sm': ' hidden sm:table-cell ',
+    'md': ' hidden md:table-cell ',
+    'lg': ' hidden lg:table-cell '
+}[props.hideOn === true ? 'lg' : props.hideOn] : '';
+
+</script>
---
+++ resources/js/Components/Tables/TableRow.vue
@@ -0,0 +1,5 @@
+<template>
+    <tr class="bg-transparent hover:bg-gray-700/25 text-gray-300 hover:text-gray-50 soft-transition group relative">
+        <slot/>
+    </tr>
+</template>
