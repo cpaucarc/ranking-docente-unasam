36d4e0f8d5d1a884b4d990f9b6c9894cd8cd8780
Frank Paucar <fcpaucar@gmail.com>
2022-10-12 22:27:51 -0500

    :lipstick: reemplaza los colores y valores de gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -15,6 +16,28 @@ module.exports = {
             fontFamily: {
                 sans: ['Titillium Web', ...defaultTheme.fontFamily.sans],
             },
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: {
+                50: '#fafafa',
+                100: '#f5f5f5',
+                200: '#e5e5e5',
+                300: '#d4d4d4',
+                400: '#a3a3a3',
+                500: '#737373',
+                600: '#525252',
+                700: '#363636',
+                800: '#242424',
+                900: '#1A1A1A',
+            },
+            red: colors.red,
+            blue: colors.blue,
+            green: colors.green,
+            amber: colors.amber,
         },
     },
 
