eedb4e89ceaf621a8f855b20c57a20c0b9691645
Frank Paucar <fcpaucar@gmail.com>
2022-10-14 16:00:54 -0500

    :lipstick: aplica las clases directamente a las etiquetas textarea, select y option

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -1,12 +1,14 @@
 <script setup>
 defineProps({
     value: String,
+    nullable: {type: Boolean, default: false}
 });
 </script>
 
 <template>
-    <label class="block font-medium text-gray-300 mb-1">
+    <label class="block font-semibold text-gray-400 mb-1">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot/></span>
+        <span v-if="nullable" class="text-red-500 ml-2 text-sm">* Opcional</span>
     </label>
 </template>
