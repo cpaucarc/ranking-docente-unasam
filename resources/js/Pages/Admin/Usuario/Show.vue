74748bfdda0ec2772e3d1f7645918f321f64ac43
apagolap <57104299+angelpagola@users.noreply.github.com>
2022-10-17 08:11:12 -0500

    :lipstick: Mejora la vista de mostrar usuario

--- resources/js/Pages/Admin/Usuario/Show.vue
+++ resources/js/Pages/Admin/Usuario/Show.vue
@@ -1,67 +1,60 @@
 <template>
     <AppLayout title="Usuario">
         <div class="grid grid-cols-2 gap-x-8 items-start">
-            <div>
-                <div class="relative w-full p-4 border border-gray-1 rounded-lg">
-                    <div class="text-center">
-                        <h2 class="text-2xl font-bold">{{ usuario.data.fullname }}</h2>
-                        <h2 class="text-lg font-semibold text-gray-2">{{ usuario.data.username }}</h2>
-                    </div>
-                    <div class="flex items-center justify-center pt-4">
-                        <img v-if="usuario.data.profile_photo_url"
-                             class="h-32 w-32 rounded-md object-cover bg-center flex-shrink-0 bg-gray-1/50 ring-4 ring-offset-4"
-                             :class="usuario.data.esta_activo? 'ring-green-4':'ring-red-4'"
-                             :src="usuario.data.profile_photo_url" :alt="usuario.data.name">
-                    </div>
-
-                    <div class="px-12 flex flex-col items-center pt-6 space-y-3">
-
-                        <span class="chip" :class="usuario.data.esta_activo ? 'bg-green-3' : 'bg-red-3'">
-                            {{ usuario.data.esta_activo ? 'Usuario habilitado' : 'Usuario deshabilitado' }}
-                        </span>
-
-                        <p v-if="usuario.dni" class="flex items-center space-x-2 text-gray-4">
-                            DNI: {{ usuario.data.dni }}
-                        </p>
-                        <a href="mailto:{{ usuario.data.email }}"
-                           class="flex items-center space-x-2 text-gray-4 hover:underline">
-                            Correo: {{ usuario.data.email }}
-                        </a>
-                        <p class="flex items-center space-x-2 text-gray-2">
-                            Fecha de registro: {{ usuario.data.created_at }}
-                        </p>
-                    </div>
-                    <div class="px-12 pt-6 space-y-2 flex flex-col items-center justify-center">
-
-                        <div v-if="!usuario.data.esta_activo"
-                             class="text-sm bg-red-3/40 p-3 rounded-md flex items-center text-gray-4">
-                            <TrashIcon class="icon-6 stroke-2 mr-3"/>
-                            Actualmente este usuario esta deshabilitado, por lo que no tendrá acceso al sistema.
-                        </div>
-
-                        <JetButton v-if="usuario.data.id !== $page.props.user.id" as="button"
-                                   :color="usuario.data.esta_activo ? 'danger' : 'success'"
-                                   :title="usuario.data.esta_activo ? 'Deshabilitar usuario' : 'Habilitar usuario'"
-                                   @click="verConfirmationModal = !verConfirmationModal">
+            <Card>
+                <div class="text-center">
+                    <h2 class="text-2xl font-bold">{{ usuario.data.fullname }}</h2>
+                    <h2 class="text-lg font-semibold text-gray-2">{{ usuario.data.username }}</h2>
+                </div>
+                <div class="flex items-center justify-center">
+                    <img v-if="usuario.data.profile_photo_url"
+                         class="h-32 w-32 rounded object-cover bg-center flex-shrink-0 bg-gray-900 ring-4 ring-offset-4"
+                         :class="usuario.data.esta_activo? 'ring-green-600':'ring-red-600'"
+                         :src="usuario.data.profile_photo_url" :alt="usuario.data.name">
+                </div>
+                <div class="flex items-center justify-center">
+                    <Boton variant="outlined" :color="usuario.data.esta_activo ? 'success' : 'danger'">
+                        {{ usuario.data.esta_activo ? 'Usuario habilitado' : 'Usuario deshabilitado' }}
+                    </Boton>
+                </div>
+                <div class="flex flex-col items-center justify-center space-y-2">
+                    <p v-if="usuario.data.dni" class="text-center space-x-2">
+                        DNI: {{ usuario.data.dni }}
+                    </p>
+                    <a href="mailto:{{ usuario.data.email }}"
+                       class="text-center space-x-2 hover:underline">
+                       {{ usuario.data.email }}
+                    </a>
+                    <p class="text-center">
+                        Fecha de registro: {{ usuario.data.created_at }}
+                    </p>
+                </div>
+                <div v-if="!usuario.data.esta_activo"
+                     class="text-sm bg-red-600 p-3 rounded-md flex items-center text-gray-4">
+                    <TrashIcon class="icon-6 stroke-2 mr-3"/>
+                    Actualmente este usuario esta deshabilitado, por lo que no tendrá acceso al sistema.
+                </div>
+                <div class="flex items-center justify-center">
+                    <Boton v-if="usuario.data.id !== $page.props.user.id" as="button"
+                           :color="usuario.data.esta_activo ? 'danger' : 'success'"
+                           :title="usuario.data.esta_activo ? 'Deshabilitar usuario' : 'Habilitar usuario'"
+                           @click="verConfirmationModal = !verConfirmationModal">
                             <span v-if="usuario.data.esta_activo" class="flex items-center">
                                 <XCircleIcon class="icon-6 mr-2"/>Deshabilitar
                             </span>
-                            <span v-else class="flex items-center">
+                        <span v-else class="flex items-center">
                                 <CheckCircleIcon class="icon-6 mr-2"/>Habilitar
-                            </span>
-                        </JetButton>
-                    </div>
+                        </span>
+                    </Boton>
                 </div>
-            </div>
+            </Card>
 
-            <div class="">
+            <Card>
                 <div class="flex justify-end mb-4">
                     <JetButton as="button" title="Asignar roles" @click="verDialogModal = !verDialogModal">
                         Asignar Roles
                     </JetButton>
                 </div>
-
-
                 <TableBasic v-if="usuario.data.roles.length > 0">
                     <template #head>
                         <TableHead>N°</TableHead>
@@ -73,7 +66,7 @@
                         <TableColumn>
                             {{ rol.name }}
                         </TableColumn>
-                        <TableColumn class="flex space-x-2">
+                        <TableColumn class="flex items-center justify-center space-x-2">
                             <Boton as="button" color="danger"
                                    title="Quitar rol"
                                    @click="quitarRol(rol.name)">
@@ -82,10 +75,8 @@
                         </TableColumn>
                     </TableRow>
                 </TableBasic>
-
-            </div>
+            </Card>
         </div>
-
         <ConfirmationModal :show="verConfirmationModal" @close="verConfirmationModal = false">
             <template #title>
                 ¿Desea {{ usuario.data.esta_activo ? 'Deshabilitar' : 'Habilitar' }} al usuario {{ usuario.name }}?
@@ -104,7 +95,7 @@
 
                 <form @submit.prevent="cambiarEstado">
                     <JetButton color="danger">
-                        {{ usuario.esta_activo ? 'Deshabilitar' : 'Habilitar' }}
+                        {{ usuario.data.esta_activo ? 'Deshabilitar' : 'Habilitar' }}
                     </JetButton>
                 </form>
             </template>
@@ -114,7 +105,6 @@
             <template #title>
                 Agregar Roles al Usuario {{ usuario.data.name }}
             </template>
-
             <template #content>
                 <TableBasic v-if="roles_sin_asignar.data.length > 0">
                     <TableRow v-for="rol in roles_sin_asignar.data" :key="rol.id">
@@ -127,11 +117,8 @@
                         </TableColumn>
                     </TableRow>
                 </TableBasic>
-
                 <JetInputError class="mt-2" :message="form.errors.roles_selected"/>
-
             </template>
-
             <template #footer>
                 <JetSecondaryButton @click="verDialogModal = false">Cancelar</JetSecondaryButton>
                 <ButtonSave @click="asignarRol" :saving="form.processing" normalText="Asignar"
@@ -166,6 +153,7 @@ import ButtonSave from "@/Components/BotonSave.vue";
 import DialogModal from "@/Components/DialogModal.vue";
 import JetInputError from '@/Components/InputError.vue';
 import Boton from "@/Components/Boton.vue";
+import Card from "@/Components/Card.vue";
 
 const props = defineProps({
     usuario: Object,
